7ca7f55d286d8e5b5c6a7053597cb18296761b54
smart-life-tech <christlightworld@gmail.com>
2023-08-02 10:39:05 +0100

    modified:   easy/easy.ino

--- easy/easy.ino
+++ easy/easy.ino
@@ -1,6 +1,6 @@
 #include <AccelStepper.h>
 #include <EasyNextionLibrary.h>
-EasyNex myNex(Serial2);  // Create an object of EasyNex class with the name < myNex >
+EasyNex myNex(Serial2); // Create an object of EasyNex class with the name < myNex >
 
 #define motorStepPin 2
 #define motorDirPin 3
@@ -8,47 +8,68 @@ int btnForward, btnReverse = 0;
 // Define the Nextion objects
 
 /*
- * Declare a button object [page id:0,component id:1, component name: "b0"].
- */
+   Declare a button object [page id:0,component id:1, component name: "b0"].
+*/
 
-char buffer[100] = { 0 };
+char buffer[100] = {0};
 
 // Initialize the stepper motor object
 AccelStepper stepper(AccelStepper::FULL4WIRE, motorStepPin, motorDirPin);
 
 // Define motor speed and acceleration
-const float maxSpeed = 1000.0;  // Adjust this value based on your motor's capabilities
-const float acceleration = 500.0;
+const float maxSpeed = 4000.0; // Adjust this value based on your motor's capabilities
+const float acceleration = 3000.0;
 /*
- * Register a button object to the touch event list.
- */
+   Register a button object to the touch event list.
+*/
 
-void setup() {
-  Serial.begin(9600);
-  myNex.begin(9600);  // Begin the object with a baud rate of 9600
-  // Set up the stepper motor properties
-  stepper.setMaxSpeed(maxSpeed);
-  stepper.setAcceleration(acceleration);
+void setup()
+{
+    Serial.begin(9600);
+    myNex.begin(9600); // Begin the object with a baud rate of 9600
+    // Set up the stepper motor properties
+    stepper.setMaxSpeed(maxSpeed);
+    stepper.setAcceleration(acceleration);
+    //  pinMode(4, INPUT_PULLUP);
+    // pinMode(5, INPUT_PULLUP);
 }
 
-void loop() {
-   btnForward = myNex.readNumber("b0.val");
-  Serial.println(btnForward);
-  btnReverse = myNex.readNumber("b1.val");
-  Serial.println(btnReverse);
-  // Check for button presses
-  if ((btnForward)) {
-    Serial.println("forward button pressed");
-    // Move the motor forward
-    stepper.setSpeed(maxSpeed);
-    stepper.runSpeed();
-  }
+void loop()
+{
+    btnForward = myNex.readNumber("b0.val");
+    // btnForward = digitalRead(4);
+    // Serial.println(btnForward);
+    btnReverse = myNex.readNumber("b1.val");
+    // btnReverse = digitalRead(5);
+    // Serial.println(btnReverse);
+    if (!btnForward && !btnReverse)
+    {
+        stepper.stop();
+    }
+    // Check for button presses
+    do
+    // ((btnForward))
+    {
+        if (myNex.readNumber("b0.val"))
+        {
+            Serial.println("forward button pressed");
+            // Move the motor forward
+            stepper.setSpeed(maxSpeed);
+            stepper.runSpeed();
+        }
+    } while (myNex.readNumber("b0.val") == 1);
 
-  if ((btnReverse)) {
-    Serial.println("reverse button pressed");
-    // Move the motor in reverse
-    stepper.setSpeed(-maxSpeed);
-    stepper.runSpeed();
-  }
-  delay(1000);
+    do
+    // ((btnReverse))
+    {
+        if (myNex.readNumber("b1.val"))
+        {
+            Serial.println("reverse button pressed");
+            digitalWrite(motorDirPin, LOW);
+            // Move the motor in reverse
+            stepper.setSpeed(-maxSpeed);
+            stepper.runSpeed();
+        }
+    } while (myNex.readNumber("b1.val") == 1);
+    delay(1000);
 }
